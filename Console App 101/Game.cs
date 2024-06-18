5c3fe11aaaa748680f2d2476c7131a86338769ec
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2024-06-18 16:43:21 +0500

    Code Structurized 👍

--- Console App 101/Game.cs
+++ Console App 101/Game.cs
@@ -1,4 +1,5 @@
-﻿using SFML.Graphics;
+﻿using Console_App_101;
+using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
 using System;
@@ -11,76 +12,33 @@ using System.Threading.Tasks;
 
 namespace Console_App_101
 {
+
+
     class Game
     {
         public RenderWindow screen;
-        static int c = 0;
+
+        Player player;
 
         uint window_width = 800;
         uint window_height = 600;
 
-        Vector2f player_start;
-        Vector2f player_new_pos;
-
-        int player_X = 10;
-        int player_Y;
-        float wlk_player_speed = 0.05f;
-        float spr_player_speed = 0.109f;
-
-        int player_sq_len = 128;
-
         int ground_size = 45;
-        int wlk_sheet_size = 896;
-        int spr_sheet_size = 1024;
-
-
-        Texture t_hero_std;
-
-        Texture t_hero_wlk;
-        int i_walk = 0;
-
-        Texture t_hero_spr;
-
-        Sprite hero_std;
-        Sprite hero_wlk;
-        Sprite hero_spr;
-
         VertexArray ground;
-
-        int animationDelay = 215;
 
         public Game()
         {
-            //Console.WriteLine("Initializing game...");
+            player = new Player(new List<Body>
+            {
+                new Body(new Sprite(new Texture("Idle.png")), 128),
+                new Body(new Sprite(new Texture("Walk.png")), 896),
+                new Body(new Sprite(new Texture("Run.png")), 1024)
+            }, 100);
 
             screen = new(new(window_width, window_height), "Gamion");
             screen.Closed += (sender, e) => screen.Close();
 
-            //Character
-            t_hero_std = new("Idle.png");
-            t_hero_wlk = new("Walk.png");
-            t_hero_spr = new("Run.png");
-
-            player_Y = (int)(window_height - ground_size - player_sq_len);
-
-            player_start = new Vector2f(player_X, player_Y);
-
-            hero_std = new Sprite(t_hero_std)
-            {
-                Position = player_start
-            };
-
-            hero_wlk = new Sprite(t_hero_wlk)
-            {
-                Position = player_start
-            };
-
-            player_new_pos = hero_wlk.Origin;
-
-            hero_spr = new(t_hero_spr)
-            {
-                Position = player_start
-            };
+            player.nxt_position = player.states[1].sprite.Origin;
 
             ground = new VertexArray(PrimitiveType.Quads, 4);
 
@@ -89,20 +47,16 @@ namespace Console_App_101
             ground[2] = new Vertex(new Vector2f(window_width, window_height - 0), Color.Black);
             ground[3] = new Vertex(new Vector2f(0, window_height - 0), Color.Black);
 
-            hero_std.TextureRect = new IntRect(0, 0, player_sq_len, player_sq_len);
-
-            hero_wlk.TextureRect = new IntRect(i_walk, 0, player_sq_len, player_sq_len);
+            player.states[0].sprite.TextureRect = new IntRect(player.ani_int, 0, player.box_lenght, player.box_lenght);
+            player.states[1].sprite.TextureRect = new IntRect(player.ani_int, 0, player.box_lenght, player.box_lenght);
+            player.states[2].sprite.TextureRect = new IntRect(player.ani_int, 0, player.box_lenght, player.box_lenght);
 
             run();
 
         }
 
-
-        int i = 1;
         public void run()
         {
-            //Console.WriteLine("Game Render : " + c);
-            c++;
 
             bool movingRight = !(Keyboard.IsKeyPressed(Keyboard.Key.LShift)) && Keyboard.IsKeyPressed(Keyboard.Key.D) || Keyboard.IsKeyPressed(Keyboard.Key.Right);
             bool movingLeft = !(Keyboard.IsKeyPressed(Keyboard.Key.LShift)) && Keyboard.IsKeyPressed(Keyboard.Key.A) || Keyboard.IsKeyPressed(Keyboard.Key.Left);
@@ -112,176 +66,52 @@ namespace Console_App_101
 
             if (sprintRight)
             {
-                int counter = 0;
-                updateOriginR();
-                while (i_walk < spr_sheet_size)
-                {
-                    screen.DispatchEvents();
-                    hero_spr.TextureRect = new IntRect(i_walk, 0, player_sq_len, player_sq_len);
-                    screen.Clear(Color.Cyan);
-                    screen.Draw(ground);
-                    screen.Draw(hero_spr);
-                    screen.Display();
-
-                    if (!(Keyboard.IsKeyPressed(Keyboard.Key.LShift) && Keyboard.IsKeyPressed(Keyboard.Key.D) || Keyboard.IsKeyPressed(Keyboard.Key.Right)))
-                    {
-                        break;
-                    }
-
-                    counter++;
-                    if (counter == animationDelay - 15)
-                    {
-                        i_walk += player_sq_len;
-                        counter = 0;
-                    }
-                    player_start.X += spr_player_speed;
-                    hero_spr.Position = player_start;
-
-
-                }
-                i_walk = 0;
+                player.runRight(player.states[2], ground, screen);
             }
 
             if (sprintLeft)
             {
-                int counter = 0;
-                updateOriginL();
-                while (i_walk < spr_sheet_size)
-                {
-                    screen.DispatchEvents();
-                    hero_spr.TextureRect = new IntRect(i_walk, 0, player_sq_len, player_sq_len);
-                    screen.Clear(Color.Cyan);
-                    screen.Draw(ground);
-                    screen.Draw(hero_spr);
-                    screen.Display();
-
-                    if (!(Keyboard.IsKeyPressed(Keyboard.Key.LShift) && Keyboard.IsKeyPressed(Keyboard.Key.A) || Keyboard.IsKeyPressed(Keyboard.Key.Left)))
-                    {
-                        break;
-                    }
-
-                    counter++;
-                    if (counter == animationDelay - 15)
-                    {
-                        i_walk += player_sq_len;
-                        counter = 0;
-                    }
-                    player_start.X -= spr_player_speed;
-                    hero_spr.Position = player_start;
-
-
-                }
-                i_walk = 0;
+                player.runLeft(player.states[2], ground, screen);
             }
 
             if (movingRight)
             {
-                int counter = 0;
-                updateOriginR();
-                while (i_walk < wlk_sheet_size)
-                {
-                    screen.DispatchEvents();
-                    hero_wlk.TextureRect = new IntRect(i_walk, 0, player_sq_len, player_sq_len);
-                    screen.Clear(Color.Cyan);
-                    screen.Draw(ground);
-                    screen.Draw(hero_wlk);
-                    screen.Display();
-
-                    //thanks brain
-                    if (!(Keyboard.IsKeyPressed(Keyboard.Key.D) || Keyboard.IsKeyPressed(Keyboard.Key.Right)))
-                    {
-                        break;
-                    }
-                    if(sprintRight || sprintLeft)
-                    {
-                        break;
-                    }
-                    //bro i loved this, i don't know how unefficient it is, but it solved the problem
-                    counter++;
-                    if (counter == animationDelay)
-                    {
-                        i_walk += player_sq_len;
-                        counter = 0;
-                    }
-                    player_start.X += wlk_player_speed;
-                    hero_wlk.Position = player_start;
-
-                }
-                i_walk = 0;
+                player.moveRight(player.states[1], ground, screen);
             }
 
             if (movingLeft)
             {
-                int counter = 0;
-                updateOriginL();
-                while (i_walk < wlk_sheet_size)
-                {
-                    screen.DispatchEvents();
-                    hero_wlk.TextureRect = new IntRect(i_walk, 0, player_sq_len, player_sq_len);
-                    screen.Clear(Color.Cyan);
-                    screen.Draw(ground);
-                    screen.Draw(hero_wlk);
-                    screen.Display();
-
-
-                    if (!(Keyboard.IsKeyPressed(Keyboard.Key.A) || Keyboard.IsKeyPressed(Keyboard.Key.Left)))
-                    {
-                        break;
-                    }
-                    if (sprintRight || sprintLeft)
-                    {
-                        break;
-                    }
-                    counter++;
-                    if (counter == animationDelay)
-                    {
-                        i_walk += player_sq_len;
-                        counter = 0;
-                    }
-                    player_start.X -= wlk_player_speed;
-                    hero_wlk.Position = player_start;
-
-
-                }
-                i_walk = 0;
+                player.moveLeft(player.states[1], ground, screen);
             }
 
             if (Keyboard.IsKeyPressed(Keyboard.Key.R))
             {
-                player_start.X = 10;
-                hero_wlk.Position = player_start;
+                player.resetCharacter();
             }
 
 
             screen.DispatchEvents();
             screen.Clear(Color.Cyan);
 
-            hero_std.Position = player_start;
-            screen.Draw(hero_std);
+            player.setPosition(player.position.X);
+            screen.Draw(player.states[0].sprite);
             screen.Draw(ground);
             screen.Display();
         }
 
-
-        private void updateOriginR()
+    }
+    
+    internal class GMain
+    {
+        static void Main(string[] args)
         {
-            hero_spr.Scale = new Vector2f(1, 1);
-            hero_spr.Origin = player_new_pos;
-            hero_wlk.Scale = new Vector2f(1, 1);
-            hero_wlk.Origin = player_new_pos;
-            hero_std.Scale = new Vector2f(1, 1);
-            hero_std.Origin = player_new_pos;
-        }
-
-        private void updateOriginL() {
-            hero_spr.Origin = new Vector2f(hero_spr.GetLocalBounds().Width, 0);
-            hero_spr.Scale = new Vector2f(-1, 1);
-            hero_wlk.Origin = new Vector2f(hero_wlk.GetLocalBounds().Width, 0);
-            hero_wlk.Scale = new Vector2f(-1, 1);
-            hero_std.Origin = new Vector2f(hero_wlk.GetLocalBounds().Width, 0);
-            hero_std.Scale = new Vector2f(-1, 1);
+            Game game = new();
+            while (game.screen.IsOpen)
+            {
+                game.run();
+            }
         }
     }
 
-    
 }
+
--- Console App 101/Player.cs
+++ Console App 101/Player.cs
@@ -1,5 +1,6 @@
 ﻿using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,39 +11,56 @@ namespace Console_App_101
 {
     class Player
     {
-        List<Body> states;
-        Vector2f position;
-        int health;
-        float walking_speed;
-        float running_speed;
+        public List<Body> states;
+        public Vector2f position;
+        public Vector2f nxt_position;
+        public int health;
+        public float walking_speed;
+        public float running_speed;
+        public int ani_int = 0;
+        public int box_lenght = 128;
+        public int animationDelay = 215;
 
-        Player()
+
+        public Player()
         {
             states = new List<Body>();
             health = 100;
             walking_speed = 0.05f;
             running_speed = 0.109f;
             position = new Vector2f(10, 427); //calculated player_Y
+            nxt_position = new Vector2f();
             foreach (Body b in states)
             {
                 b.sprite.Position = position;
             }
         }
 
-        Player(int health)
-        {
-            this.health = health;
-        }
-
-        Player(List<Body> states, int health)
+        public Player(List<Body> states, int health)
         {
             this.states = states;
             this.health = health;
+            walking_speed = 0.05f;
+            running_speed = 0.109f;
+            position = new Vector2f(10, 427); //calculated player_Y
+            nxt_position = new Vector2f();
+            foreach (Body b in states)
+            {
+                b.sprite.Position = position;
+            }
+
         }
 
-        public void addSprite(Body s)
+        bool movingRight = !(Keyboard.IsKeyPressed(Keyboard.Key.LShift)) && Keyboard.IsKeyPressed(Keyboard.Key.D) || Keyboard.IsKeyPressed(Keyboard.Key.Right);
+        bool movingLeft = !(Keyboard.IsKeyPressed(Keyboard.Key.LShift)) && Keyboard.IsKeyPressed(Keyboard.Key.A) || Keyboard.IsKeyPressed(Keyboard.Key.Left);
+
+        bool sprintRight = Keyboard.IsKeyPressed(Keyboard.Key.LShift) && Keyboard.IsKeyPressed(Keyboard.Key.D) || Keyboard.IsKeyPressed(Keyboard.Key.Right);
+        bool sprintLeft = Keyboard.IsKeyPressed(Keyboard.Key.LShift) && Keyboard.IsKeyPressed(Keyboard.Key.A) || Keyboard.IsKeyPressed(Keyboard.Key.Left);
+
+
+        public void addSprite(Sprite s)
         {
-            states.Add(s);
+            states.Add(new Body(s));
         }
 
         public void setSpeed(float wS, float rS)
@@ -59,17 +77,172 @@ namespace Console_App_101
                 b.sprite.Position = position;
             }
         }
+
+        public void moveRight(Body sp, VertexArray ground, RenderWindow screen)
+        {
+            int counter = 0;
+            updateOriginR();
+            while (ani_int < sp.sheet_size)
+            {
+                screen.DispatchEvents();
+                sp.sprite.TextureRect = new IntRect(ani_int, 0, box_lenght, box_lenght);
+                screen.Clear(Color.Cyan);
+                screen.Draw(ground);
+                screen.Draw(sp.sprite);
+                screen.Display();
+
+                //thanks brain
+                if (!(Keyboard.IsKeyPressed(Keyboard.Key.D) || Keyboard.IsKeyPressed(Keyboard.Key.Right)))
+                {
+                    break;
+                }
+                if (sprintRight || sprintLeft)
+                {
+                    break;
+                }
+                //bro i loved this, i don't know how unefficient it is, but it solved the problem
+                counter++;
+                if (counter == animationDelay)
+                {
+                    ani_int += box_lenght;
+                    counter = 0;
+                }
+                position.X += walking_speed;
+                sp.sprite.Position = position;
+
+            }
+            ani_int = 0;
+        }
+
+        public void moveLeft(Body sp, VertexArray ground, RenderWindow screen)
+        {
+            int counter = 0;
+            updateOriginL();
+            while (ani_int < sp.sheet_size)
+            {
+                screen.DispatchEvents();
+                sp.sprite.TextureRect = new IntRect(ani_int, 0, box_lenght, box_lenght);
+                screen.Clear(Color.Cyan);
+                screen.Draw(ground);
+                screen.Draw(sp.sprite);
+                screen.Display();
+
+
+                if (!(Keyboard.IsKeyPressed(Keyboard.Key.A) || Keyboard.IsKeyPressed(Keyboard.Key.Left)))
+                {
+                    break;
+                }
+                if (sprintRight || sprintLeft)
+                {
+                    break;
+                }
+                counter++;
+                if (counter == animationDelay)
+                {
+                    ani_int += box_lenght;
+                    counter = 0;
+                }
+                position.X -= walking_speed;
+                sp.sprite.Position = position;
+
+
+            }
+            ani_int = 0;
+        }
+
+        public void runRight(Body sp, VertexArray ground, RenderWindow screen)
+        {
+            int counter = 0;
+            updateOriginR();
+            while (ani_int < sp.sheet_size)
+            {
+                screen.DispatchEvents();
+                sp.sprite.TextureRect = new IntRect(ani_int, 0, box_lenght, box_lenght);
+                screen.Clear(Color.Cyan);
+                screen.Draw(ground);
+                screen.Draw(sp.sprite);
+                screen.Display();
+
+                if (!(Keyboard.IsKeyPressed(Keyboard.Key.LShift) && Keyboard.IsKeyPressed(Keyboard.Key.D) || Keyboard.IsKeyPressed(Keyboard.Key.Right)))
+                {
+                    break;
+                }
+
+                counter++;
+                if (counter == animationDelay - 15)
+                {
+                    ani_int += box_lenght;
+                    counter = 0;
+                }
+                position.X += running_speed;
+                sp.sprite.Position = position;
+            }
+            ani_int = 0;
+
+        }
+
+        public void runLeft(Body sp, VertexArray ground, RenderWindow screen)
+        {
+            int counter = 0;
+            updateOriginL();
+            while (ani_int < sp.sheet_size)
+            {
+                screen.DispatchEvents();
+                sp.sprite.TextureRect = new IntRect(ani_int, 0, box_lenght, box_lenght);
+                screen.Clear(Color.Cyan);
+                screen.Draw(ground);
+                screen.Draw(sp.sprite);
+                screen.Display();
+
+                if (!(Keyboard.IsKeyPressed(Keyboard.Key.LShift) && Keyboard.IsKeyPressed(Keyboard.Key.A) || Keyboard.IsKeyPressed(Keyboard.Key.Left)))
+                {
+                    break;
+                }
+
+                counter++;
+                if (counter == animationDelay - 15)
+                {
+                    ani_int += box_lenght;
+                    counter = 0;
+                }
+                position.X -= running_speed;
+                sp.sprite.Position = position;
+
+
+            }
+            ani_int = 0;
+        }
+
+        public void resetCharacter()
+        {
+            setPosition(10);
+        }
+
+        private void updateOriginR()
+        {
+            foreach (Body b in states)
+            {
+                b.sprite.Scale = new Vector2f(1, 1);
+                b.sprite.Origin = nxt_position;
+            }
+        }
+
+        private void updateOriginL()
+        {
+            foreach (Body b in states)
+            {
+                b.sprite.Scale = new Vector2f(-1, 1);
+                b.sprite.Origin = new Vector2f(b.sprite.GetLocalBounds().Width, 0);
+            }
+        }
+
+
     }
 
     class Body
     {
         public Sprite sprite { set; get; }
-        public String texture { set; get; }
-
-        Body()
-        {
-            setTexturetoSprite();
-        }
+        public int sheet_size = 0;
 
         Body(Sprite sprite, Texture texture)
         {
@@ -77,30 +250,15 @@ namespace Console_App_101
             this.sprite.Texture = texture;
         }
 
-        Body(Sprite sprite, string texture)
+        public Body(Sprite sprite, int sS)
         {
             this.sprite = sprite;
-            this.texture = texture;
-
-            setTexturetoSprite(sprite, texture);
+            this.sheet_size = sS;
         }
 
-        public void setTexturetoSprite()
+        public Body(Sprite sprite)
         {
-            Sprite sprite = new Sprite();
-            Texture texture = new(this.texture);
-            sprite.Texture = texture;
-        }
-
-        public void setTexturetoSprite(Sprite s, String t)
-        { 
-            Texture texture = new(t);
-            s.Texture = texture;
-        }
-
-        public void addSprite(Sprite s)
-        {
-
+            this.sprite = sprite;
         }
 
     }
